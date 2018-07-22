050e1205b5c7b31120061c0cdcf72df349117887
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-07-22 11:32:05 +0200

    missing @Qualifier annotation

--- microservice2/src/main/java/com/example/microservice2/infrastructure/rabbitmq/exchange/qualifier/CustomersExchange.java
+++ microservice2/src/main/java/com/example/microservice2/infrastructure/rabbitmq/exchange/qualifier/CustomersExchange.java
@@ -1,7 +1,10 @@
 package com.example.microservice2.infrastructure.rabbitmq.exchange.qualifier;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+
 /**
  * Created by mtumilowicz on 2018-07-16.
  */
+@Qualifier
 public @interface CustomersExchange {
 }
