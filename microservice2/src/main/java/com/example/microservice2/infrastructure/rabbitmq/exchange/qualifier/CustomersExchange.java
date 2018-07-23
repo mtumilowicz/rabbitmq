311c43121617c6412a7544e5658dfd49d1bff8be
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-07-23 19:17:07 +0200

    extending qualifier annotation

--- microservice2/src/main/java/com/example/microservice2/infrastructure/rabbitmq/exchange/qualifier/CustomersExchange.java
+++ microservice2/src/main/java/com/example/microservice2/infrastructure/rabbitmq/exchange/qualifier/CustomersExchange.java
@@ -2,9 +2,16 @@ package com.example.microservice2.infrastructure.rabbitmq.exchange.qualifier;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by mtumilowicz on 2018-07-16.
  */
 @Qualifier
+@Target({ElementType.PARAMETER, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface CustomersExchange {
 }
