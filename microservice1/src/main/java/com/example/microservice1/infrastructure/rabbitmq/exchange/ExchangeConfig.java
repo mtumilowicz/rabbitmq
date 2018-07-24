16e57caa35ec4d5da0d8014ce53b9b5d26ff244b
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-07-24 17:08:30 +0200

    redundant annotation (it's needed only with rabbit listeners)

--- microservice1/src/main/java/com/example/microservice1/infrastructure/rabbitmq/exchange/ExchangeConfig.java
+++ microservice1/src/main/java/com/example/microservice1/infrastructure/rabbitmq/exchange/ExchangeConfig.java
@@ -3,7 +3,6 @@ package com.example.microservice1.infrastructure.rabbitmq.exchange;
 import com.example.microservice1.infrastructure.rabbitmq.exchange.qualifier.CustomersExchange;
 import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.ExchangeBuilder;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
  * Created by mtumilowicz on 2018-07-16.
  */
 @Configuration
-@EnableRabbit
 public class ExchangeConfig {
     
     @Bean
