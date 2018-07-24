b0cbea75d2dbb710a347ad89cb44dc66c3fa0640
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-07-24 17:59:30 +0200

    downgrade of lombok; removing lombok experimental @FieldNameConstants

--- microservice1/src/main/java/com/example/microservice1/infrastructure/rabbitmq/exchange/ExchangeNames.java
+++ microservice1/src/main/java/com/example/microservice1/infrastructure/rabbitmq/exchange/ExchangeNames.java
@@ -1,13 +1,10 @@
 package com.example.microservice1.infrastructure.rabbitmq.exchange;
 
-import lombok.experimental.FieldNameConstants;
-
 /**
  * Created by mtumilowicz on 2018-07-24.
  */
-@FieldNameConstants(prefix = "")
 final class ExchangeNames {
-    String customers;
+    static final String CUSTOMERS = "customers";
 
     private ExchangeNames() {
     }
--- microservice2/src/main/java/com/example/microservice2/infrastructure/rabbitmq/exchange/ExchangeNames.java
+++ microservice2/src/main/java/com/example/microservice2/infrastructure/rabbitmq/exchange/ExchangeNames.java
@@ -1,13 +1,10 @@
 package com.example.microservice2.infrastructure.rabbitmq.exchange;
 
-import lombok.experimental.FieldNameConstants;
-
 /**
  * Created by mtumilowicz on 2018-07-24.
  */
-@FieldNameConstants(prefix = "")
 final class ExchangeNames {
-    String customers;
+    static final String CUSTOMERS = "customers";
 
     public ExchangeNames() {
     }
