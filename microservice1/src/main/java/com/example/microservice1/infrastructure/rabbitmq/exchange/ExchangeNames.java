355599ef1ae1f5c1659276a427d6dacb6de34eac
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-08-10 20:45:17 +0200

    ms1 - removing unnecessary keywords (already supported by lombok)

--- microservice1/src/main/java/com/example/microservice1/infrastructure/rabbitmq/exchange/ExchangeNames.java
+++ microservice1/src/main/java/com/example/microservice1/infrastructure/rabbitmq/exchange/ExchangeNames.java
@@ -1,11 +1,12 @@
 package com.example.microservice1.infrastructure.rabbitmq.exchange;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 /**
  * Created by mtumilowicz on 2018-07-24.
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 final class ExchangeNames {
     static final String CUSTOMERS = "customers";
-
-    private ExchangeNames() {
-    }
 }
